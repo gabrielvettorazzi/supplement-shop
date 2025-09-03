da3c37f1c93491beeef680095209842b9a17f6ca
gabriel <gabrielvettorazzii@gmail.com>
2025-09-03 10:29:52 -0300

    att useclient

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { Footer } from "@/components/shared/Footer";
 import { Header } from "@/components/shared/Header";
 
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { MountainIcon } from "lucide-react";
 import Link from "next/link";
 
